ed7471d6a12d151aac787205c6e6988d5ee4005b
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2026-06-04 11:44:24 -0400

    fix: cweagans/composer-patches must be installed, not just locked

--- .devpanel/init.sh
+++ .devpanel/init.sh
@@ -24,7 +24,7 @@ time sudo rm -rf lost+found
 #== Composer install.
 echo
 if [ -f composer.json ]; then
-  if composer show --locked cweagans/composer-patches ^2 &> /dev/null; then
+  if composer show cweagans/composer-patches ^2 &> /dev/null; then
     echo 'Update patches.lock.json.'
     time composer prl
     echo
